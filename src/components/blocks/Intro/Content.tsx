db02093203a3f3457b543684c502db59a1309be3
AndrewThian <andrewbobbythian@gmail.com>
2018-11-15 22:31:28 +0800

    update font size

--- src/components/blocks/Intro/Content.tsx
+++ src/components/blocks/Intro/Content.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Content = styled.div`
     font-family: 'Raleway', sans-serif;
-    font-size: 17px;
+    font-size: 14px;
 `
 
 export default Content
--- src/components/blocks/Intro/Header.tsx
+++ src/components/blocks/Intro/Header.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Header = styled.div`
     font-family: 'Raleway', sans-serif;
     font-weight: bold;
-    font-size: 20px;
+    font-size: 18px;
     margin-bottom: 5px;
 `
 
